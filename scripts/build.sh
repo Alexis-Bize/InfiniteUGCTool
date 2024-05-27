409c87bbe1d3f80cc951dcf242da954ca1ab6488
Alexis BIZE <alexis.bize@gmail.com>
2024-05-27 19:02:11 +0200

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -67,16 +67,22 @@ go generate
 for platform in "${target_platforms[@]}"
 do
 	platform_split=(${platform//\// })
+
 	GOOS=${platform_split[0]}
 	GOARCH=${platform_split[1]}
-	output_name=$package_name'-'$GOOS'-'$GOARCH
+
+	base_output_name=$package_name'-'$GOOS'-'$GOARCH
+	output_name=$base_output_name
 	if [ $GOOS = "windows" ]; then
 		output_name+='.exe'
 	fi
 
-	env GOOS=$GOOS GOARCH=$GOARCH go build -o "./build/"$output_name
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o "./build/bin/"$output_name
 	if [ $? -ne 0 ]; then
 		echo 'An error has occurred! Aborting the script execution...'
 		exit 1
 	fi
+
+	mkdir -p "./build/archive/"
+	zip -r "./build/archive/"$base_output_name".zip" "./build/bin/"$output_name
 done
